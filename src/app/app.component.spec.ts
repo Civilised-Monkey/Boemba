dfe1c65577655e24a4ef8f9aa1f5c65483d5ec25
JensDN <jens.deneef@gmail.com>
2024-12-13 22:19:17 +0100

    refactor(app): Change name of app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'boemboe' title`, () => {
+  it(`should have the 'boemba' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('boemboe');
+    expect(app.title).toEqual('boemba');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, boemboe');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, boemba');
   });
 });
